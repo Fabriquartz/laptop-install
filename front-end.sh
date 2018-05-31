6219fa9d37386ae959e01adef7f536892366256c
Chantal Broeren <chantal.broeren@fabriquartz.com>
2018-05-31 16:16:47 +0200

    Improve front-end install

--- front-end.sh
+++ front-end.sh
@@ -1,8 +1,12 @@
+# ==============================================================================
+# Frond end specific installation
+# ==============================================================================
+
 brew_install 'watchman'               # file watching service
+brew_install 'yarn'                   # yarn dependency manager
 
 npm_install 'bower'                       # bower package manager
-npm_install 'phantomjs'                   # headless browser testing
 npm_install 'ember-cli'                   # ember cli
-npm_install 'nombom'                      # something for ember
+npm_install 'nombom'                      # npm cleaner
 npm_install 'jshint'                      # js linter
-npm_install 'eslint'                      # js linter
+npm_install 'eslint'                      # es linter
